d47a52a93342a68c608089354f9e4868d83442c9
Rustin Player <rjplayer@outlook.com>
2018-08-28 16:17:15 -0600

    Created Notifications Factory

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,10 +3,7 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   describe 'creation' do
     it 'can be created' do
-        notification = Notification.create(phone: "555-555-5555",
-                                           body: "A message",
-                                           source_app: "some app"
-                                          )
+        notification = FactoryBot.create(:notification)
         expect(notification).to be_valid
     end
   end
